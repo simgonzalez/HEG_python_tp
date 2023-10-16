3407f810eadf528e86c470317be0f94c51a93e9d
Simon Gonzalez <sgonzalez@ikmail.com>
2023-10-16 16:33:26 +0200

    update exo 3

--- exercice_03/01.py
+++ exercice_03/01.py
@@ -5,7 +5,7 @@ while annee_escalade_user != ANNEE_ESCALADE:
     annee_escalade_user: int = int(input("En quelle année à eu lieu l'escalade ?"))
     if annee_escalade_user == ANNEE_ESCALADE:
         print("C'est la bonne réponse")
-    elif ANNEE_ESCALADE - 5 <= annee_escalade_user <= annee_escalade_user + 5:
+    elif ANNEE_ESCALADE - 5 <= annee_escalade_user <= ANNEE_ESCALADE + 5:
         print("C'est presque ca")
     else:
         print("Non c'est faux")
