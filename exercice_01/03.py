8835bbc9544ce779a226fbb01472479b36b9d233
Simon Gonzalez <sgonzalez@ikmail.com>
2023-10-16 15:49:24 +0200

    feat(Prog lesson): add exo 3 to the repository

---
+++ exercice_01/03.py
@@ -0,0 +1,11 @@
+ANNEE_ESCALADE: int = 1602
+annee_escalade_user:int = 0
+
+while annee_escalade_user != ANNEE_ESCALADE:
+    annee_escalade_user: int = int(input("En quelle année à eu lieu l'escalade ?"))
+    if annee_escalade_user == ANNEE_ESCALADE:
+        print("C'est la bonne réponse")
+    elif ANNEE_ESCALADE - 5 <= annee_escalade_user <= annee_escalade_user + 5:
+        print("C'est presque ca")
+    else:
+        print("Non c'est faux")
