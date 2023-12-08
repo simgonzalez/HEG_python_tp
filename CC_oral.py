7ba18b64623d534f8e99b979bab72561da386b1a
Simon Gonzalez <sgonzalez@ikmail.com>
2023-12-08 11:37:53 +0100

    pep8 guideline enforcement

--- CC_oral.py
+++ CC_oral.py
@@ -1,3 +1,4 @@
 """
 CC oral de programmation Python en version 3.11.2
+09.12.2023 salle B4.05 10h00 
 """
--- cholesterol.py
+++ cholesterol.py
@@ -12,8 +12,11 @@ while cholesterol_saisie.lower() != ACTION_FIN:
         total_valeur_cholesterol += cholesterol_saisie + ';'
     else:
         print("Le taux de cholestérol doit être compris entre 1 et 7 mmol/L !")
-moyenne_taux_choleseterol = round(sum([float(val_chol)]for val_chol in total_valeur_cholesterol.split(';')) / len(total_valeur_cholesterol.split(';')))
-print(f"La moyenne des taux dfe cholestérol est de {moyenne_taux_choleseterol:.1f}")
+
+moyenne_taux_choleseterol = round(sum([float(val_chol)]for val_chol in total_valeur_cholesterol.split(
+    ';')) / len(total_valeur_cholesterol.split(';')))
+print(
+    f"La moyenne des taux dfe cholestérol est de {moyenne_taux_choleseterol:.1f}")
 if moyenne_taux_choleseterol < 5.7:
     resultat_traitement_necessaire = "Le patient doit suivre un traitement anti-cholestérol"
 else:
